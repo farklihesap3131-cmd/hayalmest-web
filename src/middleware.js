84fcd2c8c0c8362f1244fdc6d2cfbe84f2655a6d
Tolgakabadayi <kabadayi.tolga@gmail.com>
2026-06-16 08:41:15 +0300

    Add fallback secret to middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET || "hayalmest_premium_secret_key_2026_xyz123",
   pages: {
     signIn: "/admin/login",
   },
